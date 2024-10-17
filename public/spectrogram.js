dc5f4838abbff2d3a0072c95b8fcef6a1a96b35b
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2024-10-18 04:42:29 +0700

    Create spectrogram.js

---
+++ public/spectrogram.js
@@ -0,0 +1,31 @@
+// Create an instance of WaveSurfer
+const ws = WaveSurfer.create({
+  container: '#waveform',
+  waveColor: 'rgb(200, 0, 200)',
+  progressColor: 'rgb(100, 0, 100)',
+  url: '/examples/audio/audio.wav',
+  sampleRate: 22050,
+})
+
+// Initialize the Spectrogram plugin
+ws.registerPlugin(
+  Spectrogram.create({
+    labels: true,
+    height: 200,
+    splitChannels: true,
+  }),
+)
+
+// Play on click
+ws.once('interaction', () => {
+  ws.play()
+})
+
+/*
+<html>
+  <div id="waveform"></div>
+  <p>
+    📖 <a href="https://wavesurfer.xyz/docs/classes/plugins_spectrogram.SpectrogramPlugin">Spectrogram plugin docs</a>
+  </p>
+</html>
+*/
